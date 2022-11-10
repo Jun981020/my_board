0b6f1b64403579ab09cb5a80aedd6fd45cf4933e
Jun981020 <flwnsgud@naver.com>
2022-11-10 17:19:54 +0900

    commit

---
+++ src/main/java/jproject/my_board/entity/Board.java
@@ -0,0 +1,77 @@
+package jproject.my_board.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+
+@Entity
+public class Board {
+    @Id @GeneratedValue
+    private Long id;
+
+    private String title;
+    private String content;
+    private LocalDateTime create_at;
+    private LocalDateTime update_at;
+    private boolean private_content;
+    @ManyToOne
+    private Long member_id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreate_at() {
+        return create_at;
+    }
+
+    public void setCreate_at(LocalDateTime create_at) {
+        this.create_at = create_at;
+    }
+
+    public LocalDateTime getUpdate_at() {
+        return update_at;
+    }
+
+    public void setUpdate_at(LocalDateTime update_at) {
+        this.update_at = update_at;
+    }
+
+    public boolean isPrivate_content() {
+        return private_content;
+    }
+
+    public void setPrivate_content(boolean private_content) {
+        this.private_content = private_content;
+    }
+
+    public Long getMember_id() {
+        return member_id;
+    }
+
+    public void setMember_id(Long member_id) {
+        this.member_id = member_id;
+    }
+}
