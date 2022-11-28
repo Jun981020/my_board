06db4e6278539a5ed6386ece85100f57a68f21f2
Jun981020 <flwnsgud@naver.com>
2022-11-28 21:01:12 +0900

    javascript inline 기능 미흡

---
+++ src/main/resources/static/js/myScript.js
@@ -0,0 +1,10 @@
+function check(){
+//    if(user == null){
+//      alert("게시글을 보시려면 회원가입을 먼저 해주세요");
+//      return location.href = '/member/loginform';
+//    }else{
+//      return location.href = '/board/main';
+//    }
+    alert("")
+};
+
